0616754c53a1b853ec0a50ff507e588aac8337b6
Ben Sokol <ben@bensokol.com>
2019-09-25 20:06:57 -0500

    Add comments to QUASH_main.cpp

--- src/QUASH_main.cpp
+++ src/QUASH_main.cpp
@@ -3,7 +3,7 @@
 * @Author:   Ben Sokol <Ben>
 * @Email:    ben@bensokol.com
 * @Created:  September 23rd, 2019 [8:00pm]
-* @Modified: September 25th, 2019 [6:40pm]
+* @Modified: September 25th, 2019 [8:06pm]
 * @Version:  1.0.0
 *
 * Copyright (C) 2019 by Ben Sokol. All Rights Reserved.
@@ -33,7 +33,11 @@ namespace QUASH {
       if (std::cin.eof()) {
         break;
       }
+
+      // Tokenize input string
       QUASH::Tokens tok = QUASH::Tokens(input_string);
+
+      // Print tokens from tokenizer
       tok.printTokens();
     }
 
