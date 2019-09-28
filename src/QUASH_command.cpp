a931d8a0c37f7e5c5d0791fd8d699e5709c0118d
GoldnWarir <t.shogun@ku.edu>
2019-09-28 10:32:50 -0500

    Added QUASH_command files

---
+++ src/QUASH_command.cpp
@@ -0,0 +1,1 @@
+#include "QUASH_command.hpp"
---
+++ src/QUASH_command.hpp
@@ -0,0 +1,8 @@
+/*This class should return a status from QUASH_public
+Return code is an u_int8
+Should search PATH until the executable specified is found
+Should utilize fork() and exec()
+
+*/
+
+#include "QUASH_public.hpp"
